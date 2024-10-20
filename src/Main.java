ddc8230d23c3d3817da51a2eda95e68050f5a500
Alexandre Bastos <xande.dev@outlook.com>
2024-10-20 19:22:51 -0300

    :construction: Feita lógica inicial do jogo.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author Alexandre Bastos
+ * */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        // mensagem de bem vindas do jogo, e menu para iniciar o finalizar
+        String welcomeMessage = """
+                Bem-vindo ao jogo de adivinhação de Números!
+                Digite a opção desejada:
+                
+                1 - Digite 1 Iniciar jogo.
+                2 - Digite 2 Sair do jogo.
+                """;
+        System.out.println(welcomeMessage);
+        int option = scanner.nextInt();
+        if(option == 1) {
+            System.out.println("iniciar jogo");
+        } else if(option == 2) {
+            System.exit(0);;
+        }
+
+        // Input para definir o Inicio do intervalo
+        System.out.print("Por favor defina o inicio do intervalo:");
+        int start = scanner.nextInt();
+
+        // Input para definir o Fim do intervalo
+        System.out.print("Por favor defina o fim do intervalo:");
+        int end = scanner.nextInt();
+
+        System.out.println("O intervalo escolhido está entre: " + start + " e " + end + ".");
+
+        int selectedNumber = random.nextInt(end - start + 1) + start;
+
+        while (true) {
+            System.out.println("Digite seu palpite: ");
+            int guessNumber = scanner.nextInt();
+
+            if(guessNumber > selectedNumber) {
+                System.out.println("O palpite é maior que o número sorteado");
+            } else if(guessNumber < selectedNumber) {
+                System.out.println("O palpite é menor que o número sorteado");
+            } else {
+                System.out.println("parabéns");
+                break;
+            }
+        }
+
     }
 }
