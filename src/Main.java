767cb9c8b5ce63af97eb3d999536eef7aaa2e41b
Alexandre Bastos <xande.dev@outlook.com>
2024-10-20 20:08:24 -0300

    :construction: feita lógica para contabilizar tentativas do usuario.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 /**
  * @author Alexandre Bastos
- * */
+ */
 
 public class Main {
     public static void main(String[] args) {
@@ -11,44 +11,63 @@ public class Main {
         Random random = new Random();
 
         // mensagem de bem vindas do jogo, e menu para iniciar o finalizar
-        String welcomeMessage = """
+        String msgBoasVindas = """
                 Bem-vindo ao jogo de adivinhação de Números!
                 Digite a opção desejada:
-                
+                                
                 1 - Digite 1 Iniciar jogo.
                 2 - Digite 2 Sair do jogo.
                 """;
-        System.out.println(welcomeMessage);
-        int option = scanner.nextInt();
-        if(option == 1) {
-            System.out.println("iniciar jogo");
-        } else if(option == 2) {
-            System.exit(0);;
+
+        boolean entradaValida = false;
+
+        // loop para validar opção escolhida pelo usuario
+        while (!entradaValida) {
+            System.out.println(msgBoasVindas);
+
+            try {
+                int opcao = scanner.nextInt();
+                if (opcao == 1) {
+                    System.out.println("iniciar jogo");
+                    entradaValida = true;
+                } else if (opcao == 2) {
+                    System.exit(0);
+                    entradaValida = true;
+                } else {
+                    System.out.println("Por favor digite uma das opções listadas.");
+                }
+            } catch (Exception e) {
+                System.out.println("Entrada inválida! por favor digite uma das opções.");
+                scanner.nextLine();
+            }
         }
 
         // Input para definir o Inicio do intervalo
         System.out.print("Por favor defina o inicio do intervalo:");
-        int start = scanner.nextInt();
+        int inicioDoIntervalo = scanner.nextInt();
 
         // Input para definir o Fim do intervalo
         System.out.print("Por favor defina o fim do intervalo:");
-        int end = scanner.nextInt();
+        int fimDoIntervalo = scanner.nextInt();
 
-        System.out.println("O intervalo escolhido está entre: " + start + " e " + end + ".");
+        System.out.println("O intervalo escolhido está entre: " + inicioDoIntervalo + " e " + fimDoIntervalo + ".");
 
-        int selectedNumber = random.nextInt(end - start + 1) + start;
+        int numeroSelecionado = random.nextInt(fimDoIntervalo - inicioDoIntervalo + 1) + inicioDoIntervalo;
+        int tentativas = 0;
+        boolean acertou = false;
 
-        while (true) {
+        while (!acertou) {
             System.out.println("Digite seu palpite: ");
-            int guessNumber = scanner.nextInt();
+            int palpite = scanner.nextInt();
+            tentativas++;
 
-            if(guessNumber > selectedNumber) {
+            if (palpite > numeroSelecionado) {
                 System.out.println("O palpite é maior que o número sorteado");
-            } else if(guessNumber < selectedNumber) {
+            } else if (palpite < numeroSelecionado) {
                 System.out.println("O palpite é menor que o número sorteado");
             } else {
-                System.out.println("parabéns");
-                break;
+                System.out.println("Parabéns! Você acertou em " + tentativas + " tentativas!");
+                acertou = true;
             }
         }
 
